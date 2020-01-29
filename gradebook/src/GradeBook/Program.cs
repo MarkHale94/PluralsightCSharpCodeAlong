36cdba8c5e17b36f2d49646953be25a2aa5df171
Mark Hale <mark.hale94@gmail.com>
2020-01-28 20:51:12 -0600

    Begining plurasight c# path for refresher and continued learning

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new [] {12.7, 10.3, 6.11, 4.1};
+            //double x = 31.12;
+            //double y = 42.7;
+            Console.WriteLine(numbers[0]);
+            if(args.Length > 0){
+                Console.WriteLine($"Hello, {args[0]}");
+            }
+            else{
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
