a6255ac7380242d6c2072da68c645f37baab4f70
Mark Hale <mark.hale94@gmail.com>
2020-01-28 21:10:17 -0600

    adding in lists

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,10 +8,13 @@ namespace GradeBook
         static void Main(string[] args)
         {
             //cw can be used as shorthand for the console.writeline
+            //ctrl . can be used to access the quick fixes
+
             var numbers = new [] {12.7, 10.3, 6.11, 4.1};
+            var grades = new List<double>(){12.7, 10.3, 6.11, 4.1};
 
             var result = 0.0;
-            foreach(double number in numbers){
+            foreach(double number in grades){
                 result += number;
             }
             Console.WriteLine(result);
