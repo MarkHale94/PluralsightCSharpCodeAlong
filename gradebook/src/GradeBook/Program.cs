3f97a05105b61b5395d808d7e1d78739dc1fdead
Mark Hale <mark.hale94@gmail.com>
2020-01-28 20:56:46 -0600

    updating the result add to use a foreach instead of doing it manually by hand

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,11 +9,10 @@ namespace GradeBook
             //cw can be used as shorthand for the console.writeline
             var numbers = new [] {12.7, 10.3, 6.11, 4.1};
 
-            var result = numbers[0];
-            result += numbers[1];
-            result += numbers[2];
-            result += numbers[3];
-
+            var result = 0.0;
+            foreach(double number in numbers){
+                result += number;
+            }
             Console.WriteLine(result);
 
             if(args.Length > 0){
