228df34bd03699ddc4c032871eed5a4fe1f0fe57
Mark Hale <mark.hale94@gmail.com>
2020-01-28 23:22:01 -0600

    Adding in project reference for the GradeBook project and renamed the test file name and the class name to BookTests

--- gradebook/test/GradeBook.Tests/BookTests.cs
+++ gradebook/test/GradeBook.Tests/BookTests.cs
@@ -2,10 +2,10 @@ using System;
 using Xunit;
 
 namespace GradeBook.Tests {
-    public class UnitTest1 {
+    public class BookTests {
         [Fact]
         public void Test1 () {
-
+            var book = new Book("");
         }
     }
 }
