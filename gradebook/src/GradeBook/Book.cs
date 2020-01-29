dde13f6fcdbfcd7aa11b0b82e2ddf4f8eec11664
Mark Hale <mark.hale94@gmail.com>
2020-01-28 22:16:17 -0600

    adding the ShowStatistics method to the book class, which prints to the console the lowest grade, the highest grade, and the average grade

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,17 +1,31 @@
+using System;
 using System.Collections.Generic;
 
-namespace GradeBook 
-{
-    public class Book 
-    {
-        public Book(string name){
+namespace GradeBook {
+    public class Book {
+        public Book (string name) {
             //can also use this.Name = name
             Name = name;
-            Grades = new List<double>();
+            Grades = new List<double> ();
         }
-        public void AddGrade(double grade)
-        {
-            Grades.Add(grade);
+        public void AddGrade (double grade) {
+            Grades.Add (grade);
+        }
+
+        public void ShowStatistics () {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (double number in Grades) {
+                lowGrade = Math.Min (number, lowGrade);
+                highGrade = Math.Max (number, highGrade);
+                result += number;
+            }
+            result /= Grades.Count;
+            //:N1 is a formatter that specifies that it is a number with 1 decimal place precision
+            Console.WriteLine ($"The lowest grade is {lowGrade:N1}.");
+            Console.WriteLine ($"The highest grade is {highGrade:N1}.");
+            Console.WriteLine ($"The average grade is {result:N1}.");
         }
 
         private List<double> Grades;
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -10,19 +10,14 @@ namespace GradeBook
             //cw can be used as shorthand for the console.writeline
             //ctrl . can be used to access the quick fixes
             // /= can be used like += for shorthand of math operations
+            //ctrl k+c can also be used for commenting out lines of code
+            //ctrl k+u can be used to uncomment lines
+            //static methods prevents the field from being instantiated with the object and can only be used through the class. Console.WriteLine is an example of a static method. You don't need to instantiate a new console in order to be able to use the writeline method.
+
             var book = new Book("Mark's Gradebook");
             book.AddGrade(89.1);
-
-            var grades = new List<double>(){12.7, 10.3, 6.11, 4.1};
-            grades.Add(56.1);
-
-            var result = 0.0;
-            foreach(double number in grades){
-                result += number;
-            }
-            result /= grades.Count;
-            //:N1 is a formatter that specifies that it is a number with 1 decimal place precision
-            Console.WriteLine($"The average grade is {result:N1}.");
+            book.AddGrade(42.3);
+            book.ShowStatistics();
         }
     }
 }
