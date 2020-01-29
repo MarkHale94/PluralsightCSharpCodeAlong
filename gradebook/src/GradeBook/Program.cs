0839c515e9b2d0b1986125bf2326ba59c97a9725
Mark Hale <mark.hale94@gmail.com>
2020-01-28 21:42:45 -0600

    Adding in book class

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -10,7 +10,8 @@ namespace GradeBook
             //cw can be used as shorthand for the console.writeline
             //ctrl . can be used to access the quick fixes
             // /= can be used like += for shorthand of math operations
-
+            var book = new Book();
+            book.AddGrade(89.1);
             var grades = new List<double>(){12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
 
@@ -21,13 +22,6 @@ namespace GradeBook
             result /= grades.Count;
             //:N1 is a formatter that specifies that it is a number with 1 decimal place precision
             Console.WriteLine($"The average grade is {result:N1}.");
-
-            if(args.Length > 0){
-                Console.WriteLine($"Hello, {args[0]}");
-            }
-            else{
-                Console.WriteLine("Hello");
-            }
         }
     }
 }
